18e131d151404d315ee654bf7677f9accd38c83c
Kitsadakon850 <kitsadakon850@gmail.com>
2026-09-19 16:51:25 +0700

    Update ProductOthers component and routes

--- resources/js/Pages/ProductOthers.jsx
+++ resources/js/Pages/ProductOthers.jsx
@@ -1,4 +1,5 @@
-...
+import React, { useState, useEffect } from "react";
+import BootstrapLayout from "@/layouts/BootstrapLayout";
 const ProductOthers = () => {
     const [products, setProducts] = useState([]);
 
